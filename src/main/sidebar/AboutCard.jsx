ea6ee4819575157dedbd84f3c2e0b1ce67b39022
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-23 23:36:00 +0530

    give styling to the about card

--- src/main/sidebar/AboutCard.jsx
+++ src/main/sidebar/AboutCard.jsx
@@ -12,26 +12,26 @@ import aboutImage5 from '../../assets/images/icon-design.svg'
 function AboutCard() {
   return (
     <div className="container">
-      <div data-text="Web Designer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
+      <div data-text="Web Designer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer hover:border-[1px] hover:border-[#fbb9b6]">
         
         <img src={aboutImage1} alt="" />
-        <span>High-quality design crafted to professional standards.</span>
+        <span>Focusing on creating intuitive, responsive, and visually appealing website</span>
       </div>
 
-      <div data-text="Wed Developer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
+      <div data-text="Wed Developer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer hover:border-[1px] hover:border-[#fbb9b6]">
       <img src={aboutImage2} alt="" />
         <span>Top-tier, expertly crafted website development.</span>
       </div>
-      <div data-text="Chrom Ext. dev." style={{ "--r": "5" }} className="glass hover:cursor-pointer">
+      <div data-text="Chrom Ext. dev." style={{ "--r": "5" }} className="glass hover:cursor-pointer hover:border-[1px] hover:border-[#fbb9b6]">
       <img src={aboutImage1} alt="" />
         <span>Creating custom tools to enhance browser functionality and user experience.</span>
       </div>
-      <div data-text="DSA Enthusiast" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
+      <div data-text="DSA Enthusiast" style={{ "--r": "25" }} className="glass hover:cursor-pointer hover:border-[1px] hover:border-[#fbb9b6]">
       <img src={aboutImage1} alt="" />
         <span>Dedicated to solving complex problems and optimizing solutions.</span>
       </div>
 
-      <div data-text="AWS" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
+      <div data-text="AWS" style={{ "--r": "25" }} className="glass hover:cursor-pointer hover:border-[1px] hover:border-[#fbb9b6]">
         <img src={aboutImage1} alt="" />
         <span>Proficient in AWS EC2 deployment, managing scalable and secure cloud applications.</span>
       </div>
