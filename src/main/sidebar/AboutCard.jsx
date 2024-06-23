266d3293f1bc7716792782709577731912d8aa4f
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-23 23:24:41 +0530

    write the content of eact card

--- src/main/sidebar/AboutCard.jsx
+++ src/main/sidebar/AboutCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import aboutImage1 from '../../assets/images/icon-design.svg'
 import aboutImage2 from '../../assets/images/icon-dev.svg'
 
-import aboutImage3 from '../../assets/images/icon-design.svg'
+import aboutImage3 from '../../assets/images/chrome.svg'
 
 import aboutImage4 from '../../assets/images/icon-design.svg'
 
@@ -12,28 +12,28 @@ import aboutImage5 from '../../assets/images/icon-design.svg'
 function AboutCard() {
   return (
     <div className="container">
-      <div data-text="Github" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
+      <div data-text="Web Designer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
         
         <img src={aboutImage1} alt="" />
-        <span>follow</span>
+        <span>High-quality design crafted to professional standards.</span>
       </div>
 
-      <div data-text="Github" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
+      <div data-text="Wed Developer" style={{ "--r": "-15" }} className="glass hover:cursor-pointer">
       <img src={aboutImage2} alt="" />
-        <span>follow</span>
+        <span>Top-tier, expertly crafted website development.</span>
       </div>
-      <div data-text="Code" style={{ "--r": "5" }} className="glass hover:cursor-pointer">
+      <div data-text="Chrom Ext. dev." style={{ "--r": "5" }} className="glass hover:cursor-pointer">
       <img src={aboutImage1} alt="" />
-        <span>follow</span>
+        <span>Creating custom tools to enhance browser functionality and user experience.</span>
       </div>
-      <div data-text="Earn" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
+      <div data-text="DSA Enthusiast" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
       <img src={aboutImage1} alt="" />
-        <span>follow</span>
+        <span>Dedicated to solving complex problems and optimizing solutions.</span>
       </div>
 
-      <div data-text="Earn" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
+      <div data-text="AWS" style={{ "--r": "25" }} className="glass hover:cursor-pointer">
         <img src={aboutImage1} alt="" />
-        <span>follow</span>
+        <span>Proficient in AWS EC2 deployment, managing scalable and secure cloud applications.</span>
       </div>
     </div>
   );
