bfbc65ed0a104b003f6b797ef7452f103b23a7b3
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-23 15:15:48 +0530

    add animation to the about content

--- src/main/main content/MainContent.jsx
+++ src/main/main content/MainContent.jsx
@@ -6,7 +6,7 @@ import Sidebar from "./Sidebar";
 function MainContent() {
   return (
     <div className="w-full h-full flex space-x-5 rounded-[30px] bg-[#0e0c0c] shadow-[0px_0px_30px_10px_rgb(25,25,25)] p-5">
-      <PhotoSection />
+      <PhotoSection  />
 
       <Sidebar />
     </div>
--- src/main/sidebar/About.jsx
+++ src/main/sidebar/About.jsx
@@ -5,10 +5,10 @@ import AboutCard from './AboutCard';
 
 function About() {
   return (
-    <div className=' p-10 flex flex-col space-y-5'>
+    <div className=' p-10 flex flex-col space-y-5 '>
       <div className='flex flex-col space-y-2'>
-      <div className='text-[30px] font-bold text-[rgba(255,255,255,0.8)] '>About me</div>
-        <div className='text-[20px] text-[rgba(255,255,255,0.6)]'> 
+      <div className='text-[30px] font-bold text-[rgba(255,255,255,0.8)] animate__animated animate__bounceInDown'>About me</div>
+        <div className='text-[20px] text-[rgba(255,255,255,0.6)] animate__animated animate__bounceInRight'> 
           Hello! I'm Vishal Kushwaha, currently pursuing a B.Tech in Information Technology from Harcourt Butler Technical University (HBTU), Kanpur. I have a deep passion for transforming complex problems into simple, beautiful, and intuitive designs. My goal is to craft websites that are not only functional and user-friendly but also visually appealing.
           </div> 
       </div>
@@ -16,11 +16,11 @@ function About() {
             backgroundColor: 'rgba(255,255,255,0.3)',
             width: '50%',
           }}/>
-          <div className='text-[30px] font-bold text-[rgba(255,255,255,0.8)] '>
+          <div className='text-[30px] font-bold text-[rgba(255,255,255,0.8)] animate__animated animate__bounceIn '>
             What I'm Doing
             </div> 
-            <div>
-              <AboutCard/>
+            <div >
+              <AboutCard />
               </div>    
     </div>
   )
