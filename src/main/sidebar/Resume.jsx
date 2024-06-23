7495dd1cd09738ecdad622959891d4796411e075
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-24 00:57:33 +0530

    add time line in the resume component

--- src/main/sidebar/Resume.jsx
+++ src/main/sidebar/Resume.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Timeliner from './Timeliner'
 
 function Resume() {
   return (
-    <div>
-      resume
+    <div className='flex w-full  bg-white items-center'>
+      <div className='bg-red-500 w-1/2' >
+        <Timeliner/>
+      </div>
+      <div className='bg-green-500 w-1/2 overflow-clip '>sdjgjlkdfjgldjlgjlfsf</div>
     </div>
   )
 }
