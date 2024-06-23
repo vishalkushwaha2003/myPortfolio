cced828786977a2b4db40aeeff26081152b9da33
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-24 05:24:36 +0530

    add time line with education content

--- src/main/sidebar/Resume.jsx
+++ src/main/sidebar/Resume.jsx
@@ -5,7 +5,7 @@ function Resume() {
   return (
     <div className=" w-full h-[90%] p-5 grid grid-cols-2 grid-rows-12 gap-1">
    
-      <div className="col-span-1 row-span-10 bg-gray-400">Item 1</div>
+      <div className="col-span-1 row-span-10  p-5 flex items-center justify-center "><Timeliner/></div>
       <div className="col-span-1 row-span-10 bg-gray-400">Item 2</div>
       <div className="col-span-2 row-span-2 bg-gray-600">Item 3</div>
     </div>
--- src/main/sidebar/Timeliner.jsx
+++ src/main/sidebar/Timeliner.jsx
@@ -5,36 +5,83 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import Tip from './Tip';
+import Tooltip from '@mui/material/Tooltip';
 
 export default function Timeliner() {
   return (
     <Timeline position="alternate">
       <TimelineItem>
+        
+        <TimelineSeparator>
+          {/* <TimelineDot /> */}
+          <MenuBookIcon sx={{
+            color:'rgba(255,255,255)'
+          }}/>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{
+          color:'rgba(255,255,255)',
+          fontSize:'20px',
+          fontWeight:'500'
+        }}>Education</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="rgba(251, 185, 182,0.5)">
+          2018
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          {/* <Tip/> */}
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{
+          color:'rgba(255,255,255,0.9)',
+          fontSize:'18px',
+          fontWeight:'400',
+          fontFamily:'unset'
+        }}>
+
+<Tooltip title="from Jawahar Navodaya Vidyalaya" placement="top-start" className='hover:cursor-pointer hover:text-white'>Matriculation</Tooltip>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="rgba(251, 185, 182,0.5)">
+          2020
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent sx={{
+          color:'rgba(255,255,255,0.9)',
+          fontSize:'18px',
+          fontWeight:'400',
+          fontFamily:'unset'
+        }}>
+<Tooltip title="from Jawahar Navodaya Vidyalaya" placement="top-start" className='hover:cursor-pointer hover:text-white'>Intermediate</Tooltip>
+
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+        <TimelineOppositeContent color="rgba(251, 185, 182,0.5)">
+          2021-2022
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
+        <TimelineContent sx={{
+           color:'rgba(255,255,255,0.9)',
+           fontSize:'18px',
+           fontWeight:'400',
+           fontFamily:'unset'
+        }}>
+<Tooltip title="from Harcourt Butler Technical University" placement="top-start" className='hover:cursor-pointer hover:text-white'>Graduation</Tooltip>
+
+        </TimelineContent>
       </TimelineItem>
     </Timeline>
   );
