c6a1c5024acae99e585e245491e49657f3deea8f
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-24 01:12:09 +0530

    set grid layout to the resume

--- src/main/sidebar/Resume.jsx
+++ src/main/sidebar/Resume.jsx
@@ -3,12 +3,13 @@ import Timeliner from './Timeliner'
 
 function Resume() {
   return (
-    <div className='flex w-full  bg-white items-center'>
-      <div className='bg-red-500 w-1/2' >
-        <Timeliner/>
-      </div>
-      <div className='bg-green-500 w-1/2 overflow-clip '>sdjgjlkdfjgldjlgjlfsf</div>
+    <div className=" w-full h-5/6 p-5 grid grid-cols-2 grid-rows-2 gap-1">
+   
+      <div className="col-span-1 row-span-1 bg-gray-400">Item 1</div>
+      <div className="col-span-1 row-span-1 bg-gray-400">Item 2</div>
+      <div className="col-span-2 row-span-1 bg-gray-600">Item 3</div>
     </div>
+  
   )
 }
 
