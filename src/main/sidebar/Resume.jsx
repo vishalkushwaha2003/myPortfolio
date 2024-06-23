562b2ec7b7f396f1a2016b8dfddc21920de4249f
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-24 01:15:49 +0530

    make responsive layout of the resume

--- src/main/sidebar/Resume.jsx
+++ src/main/sidebar/Resume.jsx
@@ -3,11 +3,11 @@ import Timeliner from './Timeliner'
 
 function Resume() {
   return (
-    <div className=" w-full h-5/6 p-5 grid grid-cols-2 grid-rows-2 gap-1">
+    <div className=" w-full h-[90%] p-5 grid grid-cols-2 grid-rows-12 gap-1">
    
-      <div className="col-span-1 row-span-1 bg-gray-400">Item 1</div>
-      <div className="col-span-1 row-span-1 bg-gray-400">Item 2</div>
-      <div className="col-span-2 row-span-1 bg-gray-600">Item 3</div>
+      <div className="col-span-1 row-span-10 bg-gray-400">Item 1</div>
+      <div className="col-span-1 row-span-10 bg-gray-400">Item 2</div>
+      <div className="col-span-2 row-span-2 bg-gray-600">Item 3</div>
     </div>
   
   )
