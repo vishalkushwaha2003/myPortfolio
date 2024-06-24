7a4f64ac1aa3a486271d0bd2050db4a01e5345e2
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-24 14:18:35 +0530

    add resume button

--- src/main/sidebar/Resume.jsx
+++ src/main/sidebar/Resume.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Timeliner from './Timeliner'
-import BarChart from './BarChart'
+import React from "react";
+import Timeliner from "./Timeliner";
+import BarChart from "./BarChart";
 
 function Resume() {
   return (
     <div className=" w-full h-[90%] p-5 grid grid-cols-2 grid-rows-12 gap-1">
-   
-      <div className="col-span-1 row-span-10  p-5 flex items-center justify-center "><Timeliner/></div>
-      <div className="col-span-1 row-span-10 flex flex-col justify-evenly items-center p-5">
-      <div className='text-[20px] font-medium text-white '>My Skill</div>
-      <BarChart/>
-        
+      <div className="col-span-1 row-span-10  p-5 flex items-center justify-center ">
+        <Timeliner />
       </div>
-      <div className="col-span-2 row-span-2 bg-gray-600">Item 3</div>
+      <div className="col-span-1 row-span-10 flex flex-col justify-evenly items-center p-5">
+        <div className="text-[20px] font-medium text-white ">My Skill</div>
+        <BarChart />
+      </div>
+      <div className="col-span-2 row-span-2  flex justify-center items-center">
+        <button className="resumeButton">See Full Resume</button>
+      </div>
     </div>
-  
-  )
+  );
 }
 
-export default Resume
+export default Resume;
