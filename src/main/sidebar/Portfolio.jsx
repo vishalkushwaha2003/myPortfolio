b243ed2abb87738ece0dd5cacdbdc0e067f6b1b6
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-25 11:17:00 +0530

    design grid layout for porfolio section

--- src/main/sidebar/Portfolio.jsx
+++ src/main/sidebar/Portfolio.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div>
-      portforli
+    <div className='bg-red-500 h-full w-full grid grid-cols-12 grid-rows-12'>
+      <div className='col-span-12 row-span-1 bg-slate-400'></div>
+      <div className='col-span-12 row-span-11 bg-slate-800'></div>
+      
     </div>
   )
 }
