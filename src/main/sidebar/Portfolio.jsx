ce0fe2194d9cf71500b440978a9a590b6bd07517
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-25 11:47:24 +0530

    style button in portfolio section

--- src/main/sidebar/Portfolio.jsx
+++ src/main/sidebar/Portfolio.jsx
@@ -1,9 +1,25 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 function Portfolio() {
   return (
-    <div className='bg-red-500 h-full w-full grid grid-cols-12 grid-rows-12'>
-      <div className='col-span-12 row-span-1 bg-slate-400'></div>
+    <div className=' h-full w-full grid grid-cols-12 grid-rows-12'>
+      <div className='col-span-12 row-span-1  flex justify-start items-start px-5 space-x-3'>
+
+        <Button variant='text' size='small' sx={{
+          
+          color:'whitesmoke'
+        }}>All</Button>
+        <Button variant='text'size='small' sx={{
+          
+          color:'whitesmoke'
+        }}>web Development
+        </Button>
+        <Button variant='text' size='small' sx={{
+          
+          color:'whitesmoke'
+        }}>Chrome Extension</Button>
+      </div>
       <div className='col-span-12 row-span-11 bg-slate-800'></div>
       
     </div>
