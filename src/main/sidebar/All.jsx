52bffa5e549ff3660f22057b7cbe54350190a590
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-27 13:23:32 +0530

    add animation to the porfolio chard

--- src/main/sidebar/All.jsx
+++ src/main/sidebar/All.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import PortfolioChard from './PortfolioChard'
+import React from "react";
+import PortfolioChard from "./PortfolioChard";
 
 function All() {
+  const data = ["chat App", "Activity Tracker", "FunQrew", "Runner"];
 
-  const data = ['chat App','Activity Tracker','FunQrew','Runner']
-    
   return (
-    <div className='w-full h-full p-5 justify-evenly flex items-center'>
-      <PortfolioChard data={data[0]}/>
-      <PortfolioChard data={data[1]}/>
-      <PortfolioChard data={data[2]}/>
-      <PortfolioChard data={data[3]}/>
+    <div className="w-full h-full p-5 justify-evenly flex items-center">
+      <div className="animate__animated animate__fadeInLeft">
+        <PortfolioChard data={data[0]} />
+      </div>
+      <div className="animate__animated animate__fadeInUp">
+        <PortfolioChard data={data[1]} />
+      </div>
+
+      <div className="animate__animated animate__fadeInUp">
+        <PortfolioChard data={data[2]} />
+      </div>
+      <div className="animate__animated animate__fadeInRight">
+        <PortfolioChard data={data[3]} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default All
+export default All;
