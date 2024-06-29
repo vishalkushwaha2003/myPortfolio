5141f51227afbdf8865991c542f2760aabfd8716
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-29 21:49:44 +0530

    fetch the profile data from git hub rest api

--- src/main/sidebar/LeetcodeData.jsx
+++ src/main/sidebar/LeetcodeData.jsx
@@ -28,7 +28,7 @@ const LeetcodeData = () => {
 
   console.log(data)
 
-  if (true) {
+  if (loading) {
     return <div className='col-span-1 row-span-1 '><Skeleton/></div>;
   }
 
@@ -37,15 +37,22 @@ const LeetcodeData = () => {
   }
 
   return (
-    <div className='grid grid-cols-1 grid-rows-3 w-full'>
-        {false && <div className='col-span-1 row-span-1 grid grid-cols-12 grid-rows-1'>
-             <div className='col-span-3 row-span-1'>
-                 
+    <div className='grid grid-cols-1 grid-rows-3 w-full px-5 h-full overflow-y-scroll'>
+        {data && <div className='col-span-1 row-span-1 grid grid-cols-12 grid-rows-1 w-[80%]'>
+             <div className='col-span-3 row-span-1 bg-red-700 flex justify-center items-center '>
+                    <img src={data.avatar_url} alt="avatar" className='h-[100%] rounded-full' />
              </div>
-             <div className='col-span-7 row-span-1 '>
+             <div className='col-span-7 row-span-1 bg-green-700 flex flex-col justify-center items-center'>
+                   <div className='text-[25px] font-bold text-white'>{data.name}</div>
+                   <div className='text-[15px] text-gray-400'>{data.login}</div>
+                   <div className='flex gap-3'>
+                   <div className='text-[15px] text-gray-400'>followers { data.followers} ,</div>
+                   <div className='text-[15px] text-gray-400'>following { data.following}</div>
+                   </div>
+                   <div className='text-[15px] text-gray-400'>vishalkushwaha2003@gmail.com</div>
 
              </div>
-             <div className='col-span-2 row-span-1'>
+             <div className='col-span-2 row-span-1 bg-red-600'>
                    
              </div>
         
