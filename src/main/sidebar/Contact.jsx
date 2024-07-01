3b7211e29007db2b7777e00e55515cfc4446d348
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-02 05:09:20 +0530

    styling the 3d character

--- src/main/sidebar/Contact.jsx
+++ src/main/sidebar/Contact.jsx
@@ -4,9 +4,14 @@ import Character3d from './Character3d'
 function Contact() {
   return (
     <div className='w-full h-full grid grid-cols-12 grid-rows-12'>
-      <div className='col-span-6 row-span-1 bg-red-500'></div>
-      <div className='col-span-6 row-span-1 bg-green-600'></div>
-      <div className='col-span-12 row-span-11 bg-slate-500'><Character3d/></div>
+      <div className='col-span-12 row-span-8 overflow-hidden '><Character3d/></div>
+      <div className='col-span-12 row-span-4 grid grid-cols-4 grid-rows-2 '>
+      <div className='col-span-2 row-span-1 bg-red-500'></div>
+      <div className='col-span-2 row-span-1 bg-green-600'></div>
+      <div className='col-span-3 row-span-1 bg-slate-50'></div>
+      <div className='col-span-1 row-span-1 bg-orange-500'></div>
+      </div>
+      
     </div>
   )
 }
