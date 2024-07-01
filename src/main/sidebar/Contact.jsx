798e5ed498d48a8e5691d5aa83a36fd92f2e6192
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-02 03:59:58 +0530

    add 3d character

--- src/main/sidebar/Contact.jsx
+++ src/main/sidebar/Contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Character3d from './Character3d'
 
 function Contact() {
   return (
-    <div>
-      contact
+    <div className='w-full h-full grid grid-cols-12 grid-rows-12'>
+      <div className='col-span-6 row-span-1 bg-red-500'></div>
+      <div className='col-span-6 row-span-1 bg-green-600'></div>
+      <div className='col-span-12 row-span-11 bg-slate-500'><Character3d/></div>
     </div>
   )
 }
