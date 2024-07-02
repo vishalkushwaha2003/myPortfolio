5b8f0674e816437aa636096b36de159643f11539
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-02 23:54:41 +0530

    add loader

--- src/main/sidebar/Character3d.jsx
+++ src/main/sidebar/Character3d.jsx
@@ -1,19 +1,21 @@
 // src/SplineViewer.js
 import React from 'react';
+import Loader from './Loader';
 
 const Character3d = () => {
   return (
-    <div className='w-1/2 h-full m-auto'>
+    <div className='w-[60%]  h-full m-auto overflow-hidden flex justify-center items-start'>
       <spline-viewer 
         hint 
-        loading-anim-type="spinner-big-light" 
         url="https://prod.spline.design/Jp2fE2upCjWifr97/scene.splinecode"
         logo={false}
         height="100%"
         width="100%"
         
       >
-        
+        <div className=" w-full h-full flex  justify-center items-center  ">
+          <Loader />
+        </div>
       </spline-viewer>
     </div>
   );
---
+++ src/main/sidebar/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className="🤚">
+	<div className="👉"></div>
+	<div className="👉"></div>
+	<div className="👉"></div>
+	<div className="👉"></div>
+	<div className="🌴"></div>		
+	<div className="👍"></div>
+</div>
+  )
+}
+
+export default Loader
