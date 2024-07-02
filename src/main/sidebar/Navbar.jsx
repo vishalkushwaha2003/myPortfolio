9a070d28c2beffe164d71eb9b9caa76d56e61904
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-03 02:09:40 +0530

    fix bug in the nav bar button

--- src/main/sidebar/Navbar.jsx
+++ src/main/sidebar/Navbar.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import Button from './Button';
+import React, { useState } from 'react';
 
 function Navbar({ setActiveComponent }) {
+  const [activeButton, setActiveButton] = useState('');
+
+  const handleButtonClick = (component) => {
+    setActiveComponent(component);
+    setActiveButton(component);
+  };
+
   return (
-    <nav className="flex justify-center space-x-5  items-center ">
-      
-     
-        
-          <button className="buttonNav" onClick={() => setActiveComponent('About')}>About</button>
-       
-        
-          <button className="buttonNav" onClick={() => setActiveComponent('Resume')}>Resume</button>
-       
-        
-          <button className="buttonNav" onClick={() => setActiveComponent('Portfolio')}>Portfolio</button>
-        
-        
-          <button className="buttonNav" onClick={() => setActiveComponent('Blog')}>Blog</button>
-        
-        
-          <button className="buttonNav" onClick={() => setActiveComponent('Contact')}>Contact</button>
-        
-      
+    <nav className="flex justify-center space-x-5 items-center">
+      <button
+        className={`buttonNav ${activeButton === 'About' ? 'active' : ''}`}
+        onClick={() => handleButtonClick('About')}
+      >
+        About
+      </button>
+      <button
+        className={`buttonNav ${activeButton === 'Resume' ? 'active' : ''}`}
+        onClick={() => handleButtonClick('Resume')}
+      >
+        Resume
+      </button>
+      <button
+        className={`buttonNav ${activeButton === 'Portfolio' ? 'active' : ''}`}
+        onClick={() => handleButtonClick('Portfolio')}
+      >
+        Portfolio
+      </button>
+      <button
+        className={`buttonNav ${activeButton === 'Blog' ? 'active' : ''}`}
+        onClick={() => handleButtonClick('Blog')}
+      >
+        Blog
+      </button>
+      <button
+        className={`buttonNav ${activeButton === 'Contact' ? 'active' : ''}`}
+        onClick={() => handleButtonClick('Contact')}
+      >
+        Contact
+      </button>
     </nav>
   );
 }
