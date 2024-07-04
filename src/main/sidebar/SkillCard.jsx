087c77345da24593639bf727a2ca2feb6482157e
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-05 04:31:52 +0530

    change the formate of experience

--- src/main/sidebar/SkillCard.jsx
+++ src/main/sidebar/SkillCard.jsx
@@ -10,7 +10,7 @@ const SkillCard = ({ icon,name,experience,progress}) => {
       <div className='h-full  w-full grid grid-cols-1 grid-rows-10  p-1 overflow-hidden'>
         <div className='col-span-1 row-span-4 flex justify-center items-center  text-[rgba(251,185,182,1)]'>{icon}</div>
         <div className='col-span-1 row-span-2 flex justify-center items-start font-bold text-lg text-[#eee4e4be] '>{name}</div>
-        <div className=' col-span-1 row-span-2 flex justify-center items-center text-md text-[#efeaea6b]'>{experience} year experience </div>
+        <div className=' col-span-1 row-span-2 flex justify-center items-center text-md text-[#efeaea6b]'>{experience} experience </div>
         
 
         <div className='col-span-1 row-span-2 mt-1'><ProgressBar progress={progress}/></div>
