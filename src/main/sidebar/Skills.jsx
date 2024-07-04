fe84eeb0711714253c72f3d6a35eaf415a8695ca
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-04 21:25:41 +0530

    add animation to the each card of the skill sets

--- src/main/sidebar/Skills.jsx
+++ src/main/sidebar/Skills.jsx
@@ -6,15 +6,15 @@ function Blog() {
     <div className='w-full h-full p-5'>
 
 <div className='w-full h-full grid grid-cols-3  grid-rows-3 gap-3 '>
-       <div className='col-span-1 row-span-1 border-[0.5px] border-slate-600/50 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-gray-950 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>d</div>
-       <div className='col-span-1 row-span-1 bg-zinc-800 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div> 
-       <div className='col-span-1 row-span-1 bg-slate-950 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-red-500 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-slate-400 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-orange-500 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-amber-900 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
-       <div className='col-span-1 row-span-1 bg-green-800 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.3)] duration-200'>df</div>
+       <div className='col-span-1 row-span-1 border-[0.5px] border-slate-600/50 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopLeft animate__faster'>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg  border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInDown animate__faster'>d</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopRight animate__faster'>df</div> 
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInLeft animate__faster '>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__zoomIn animate__faster'>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInRight animate__faster'>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomLeft animate__faster'>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInUp animate__faster'>df</div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomRight animate__faster'>df</div>
     </div>
     </div>
   )
