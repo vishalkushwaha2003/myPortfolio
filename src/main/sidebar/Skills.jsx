b64bba3eb12d363bfae5bcb00b3a8eb04000fbc1
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-05 04:53:02 +0530

    fix bugs

--- src/main/sidebar/Skills.jsx
+++ src/main/sidebar/Skills.jsx
@@ -14,7 +14,7 @@ function Blog() {
   return (
     <div className='w-full h-full p-5'>
 
-<div className='w-full h-full grid grid-cols-3  grid-rows-3 gap-3 overflow-hidden '>
+<div className='w-full h-full grid grid-cols-3  grid-rows-3 gap-3 '>
        <div className='col-span-1 row-span-1 border-[0.5px] border-slate-600/50 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopLeft animate__faster'><SkillCard icon={<FaHtml5 className='h-[80%] w-[80%]'/>} name={'HTML'} experience={"2 years"} progress={80}/></div>
        <div className='col-span-1 row-span-1  rounded-lg  border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInDown animate__faster'><SkillCard icon={<IoLogoCss3 className='h-[80%] w-[80%]'/>} name={'CSS'} experience={"2 years"} progress={60}/></div>
        <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopRight animate__faster'><SkillCard icon={<IoLogoJavascript className='h-[80%] w-[80%]'/>} name={'JavaScript'} experience={"1 year"} progress={55}/></div> 
