fff2319bc6eae756d0d1fdd9836b68f05e912224
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-05 04:13:37 +0530

    update each skill card content through prop drilling

--- src/main/sidebar/ProgressBar.jsx
+++ src/main/sidebar/ProgressBar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function ProgressBar() {
+function ProgressBar({progress}) {
   return (
     <div className="px-2 flex flex-col">
       <div className="w-full bg-[rgba(64,65,66,0.46)] rounded-full h-1 p">
         <div
-          className="bg-[#fbb9b6] h-1 rounded-full"
-          style={{ width: `20%` }}
+          className="bg-[rgba(251,185,182,0.81)] h-1 rounded-full"
+          style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <div className="grid grid-cols-2 grid-rows-2 text-xs text-[rgba(221,201,201,0.9)]">
+      <div className="grid grid-cols-2 grid-rows-2 text-xs text-[rgba(221,201,201,0.65)]">
         <div className=" col-span-1 row-span-1 text-left ">Beg</div>
         <div className=" col-span-1 row-span-1 text-right">Adv</div>
       </div>
--- src/main/sidebar/Skills.jsx
+++ src/main/sidebar/Skills.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import SkillCard from './SkillCard'
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { SiMui } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FiGithub } from "react-icons/fi";
+import { FaNode } from "react-icons/fa";
+import { RiBlenderLine } from "react-icons/ri";
 
 function Blog() {
   return (
     <div className='w-full h-full p-5'>
 
 <div className='w-full h-full grid grid-cols-3  grid-rows-3 gap-3 '>
-       <div className='col-span-1 row-span-1 border-[0.5px] border-slate-600/50 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopLeft animate__faster'><SkillCard/></div>
-       <div className='col-span-1 row-span-1  rounded-lg  border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInDown animate__faster'>d</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopRight animate__faster'>df</div> 
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInLeft animate__faster '>df</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__zoomIn animate__faster'>df</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInRight animate__faster'>df</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomLeft animate__faster'>df</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInUp animate__faster'>df</div>
-       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomRight animate__faster'>df</div>
+       <div className='col-span-1 row-span-1 border-[0.5px] border-slate-600/50 rounded-lg hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopLeft animate__faster'><SkillCard icon={<FaHtml5 className='h-[80%] w-[80%]'/>} name={'HTML'} experience={2} progress={80}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg  border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInDown animate__faster'><SkillCard icon={<IoLogoCss3 className='h-[80%] w-[80%]'/>} name={'CSS'} experience={2} progress={60}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInTopRight animate__faster'><SkillCard icon={<IoLogoJavascript className='h-[80%] w-[80%]'/>} name={'JavaScript'} experience={1} progress={55}/></div> 
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInLeft animate__faster '><SkillCard icon={<FaReact className='h-[80%] w-[80%]'/>} name={'REACT'} experience={1} progress={70}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__zoomIn animate__faster'><SkillCard icon={<SiMui className='h-[80%] w-[80%]'/>} name={'Material UI'} experience={1} progress={40}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInRight animate__faster'><SkillCard icon={<RiTailwindCssFill className='h-[80%] w-[80%]'/>} name={'Tailwind CSS'} experience={1} progress={80}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomLeft animate__faster'><SkillCard icon={<FiGithub className='h-[80%] w-[80%]'/>} name={'GitHub'} experience={1} progress={40}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInUp animate__faster'><SkillCard icon={<FaNode className='h-[80%] w-[80%]'/>} name={'Node.js'} experience={1} progress={50}/></div>
+       <div className='col-span-1 row-span-1  rounded-lg border-[0.5px] border-slate-600/50 hover:shadow-[0px_0px_10px_1px_rgba(241,176,176,0.15)] duration-200 animate__animated animate__fadeInBottomRight animate__faster'><SkillCard icon={<RiBlenderLine className='h-[80%] w-[80%]'/>} name={'Blender'} experience={1} progress={40}/></div>
     </div>
     </div>
   )
