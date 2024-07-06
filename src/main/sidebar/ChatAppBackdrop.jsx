9e7cdff7bde4d89b86bd9672bf9761444ed8082e
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-07 02:56:26 +0530

    make text of the chat backdrop responsive

--- src/main/sidebar/ChatAppBackdrop.jsx
+++ src/main/sidebar/ChatAppBackdrop.jsx
@@ -11,13 +11,13 @@ function ChatAppBackdrop() {
   return (
     <div className="w-full h-full  flex justify-center items-center">
       <div className="lg:w-[50%] lg:h-[80%] sm:w-[70%] sm:h-[80%] w-[96%] h-[96%] bg-[rgba(251,185,182,0.05)] rounded-xl flex flex-col">
-        <div className="w-[100%] h-[10%] flex justify-center items-center text-3xl font-bold">
+        <div className="w-[100%] h-[10%] flex justify-center items-center sm:text-3xl text-2xl font-bold">
           <a href="https://chatting-app-ouxe.onrender.com" target="_blank" className="hover:text-blue-700"><ChatIcon/>  Chat App <OpenInNewIcon/></a>
         </div>
         <div className="h-[90%] w-full p-2 flex flex-col space-y-4 overflow-scroll">
           <div className="text-wrap flex flex-col space-y-4">
-            <div className="text-3xl">Introduction</div>
-            <div className="text-xl">
+            <div className="sm:text-3xl text-2xl">Introduction</div>
+            <div className="sm:text-xl text-lg">
               The chat application is a dynamic and interactive platform that
               allows users to engage in real-time conversations with one
               another. This project integrates several modern web development
@@ -64,8 +64,8 @@ function ChatAppBackdrop() {
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">User Registration and Authentication</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">User Registration and Authentication</div>
+          <div className="sm:text-xl text-lg">
           The first step for any new user is the signup process. During signup, users are required to provide their full name, a unique username, and a password. This information is essential for creating a personalized user account and ensuring secure access to the chat platform. The password is securely hashed using bcrypt before being stored in the MongoDB database, ensuring that user credentials are protected.
  <br />
 For users who have already signed up, the application offers a straightforward login process. By entering their username and password, users can quickly access their accounts and continue their conversations without the need to sign up again.
@@ -73,8 +73,8 @@ For users who have already signed up, the application offers a straightforward l
           </div>
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Real-Time Messaging with Socket.IO</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Real-Time Messaging with Socket.IO</div>
+          <div className="sm:text-xl text-lg">
           One of the standout features of the chat application is its ability to facilitate real-time messaging. This is achieved using Socket.IO, a powerful library that enables bidirectional communication between the client and server. With Socket.IO, messages are instantly transmitted between users, creating a seamless and responsive chatting experience.
           </div>
           </div>
@@ -82,8 +82,8 @@ For users who have already signed up, the application offers a straightforward l
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Backend and Database Management</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Backend and Database Management</div>
+          <div className="sm:text-xl text-lg">
           The backend of the application is built using Express, a popular web application framework for Node.js. Express provides a robust foundation for handling various server-side operations, including user authentication, message routing, and database interactions.
            <br />
            For database management, MongoDB is employed due to its flexibility and scalability. MongoDB stores user information, chat history, and other essential data in a NoSQL format, which is well-suited for applications requiring high performance and scalability.
@@ -92,24 +92,24 @@ For users who have already signed up, the application offers a straightforward l
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Deployment on Render</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Deployment on Render</div>
+          <div className="sm:text-xl text-lg">
           Deploying the chat application is streamlined through Render, a platform that offers free deployment options with just a single click. Render simplifies the process of getting the application online, making it accessible to users without the need for complex deployment procedures. This ensures that the application is always available and running smoothly.      
              
          </div>
           </div>
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Online Status Indicator</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Online Status Indicator</div>
+          <div className="sm:text-xl text-lg">
           To provide users with information about the availability of their contacts, the chat application includes an online status indicator. A green dot next to a user’s name signifies that they are currently online and available for chat. This feature enhances the real-time aspect of the application, allowing users to know when their friends or contacts are active.             
          </div>
           </div>
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Signup and Login Process</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Signup and Login Process</div>
+          <div className="sm:text-xl text-lg">
           <b>Signup :</b> During signup, the user provides their full name, username, and password. The password is hashed using bcrypt, which ensures that even if the database is compromised, the actual passwords remain secure. The hashed password, along with other user details, is then stored in the MongoDB database.         
          <br></br> <b>Login :</b> For returning users, the login process is simple. Users input their username and password, which are validated against the stored credentials in the database. Upon successful authentication, users can access their accounts and resume chatting.
           </div>
@@ -117,8 +117,8 @@ For users who have already signed up, the application offers a straightforward l
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">User Interface Design</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">User Interface Design</div>
+          <div className="sm:text-xl text-lg">
           <b>Navigation Bar :</b> Provides quick access to different sections of the application.
              <br/>
            <b> Chat Window:</b> Displays the conversation with a selected contact. Messages are shown in a threaded format for easy readability.
@@ -133,8 +133,8 @@ For users who have already signed up, the application offers a straightforward l
 
 
           <div className="text-wrap flex flex-col space-y-4">
-          <div className="text-3xl">Conclusion</div>
-          <div className="text-xl">
+          <div className="sm:text-3xl text-2xl">Conclusion</div>
+          <div className="sm:text-xl text-lg">
           The chat application project leverages a combination of modern technologies to deliver a robust and engaging platform for real-time communication. From the secure signup and login process to the real-time messaging powered by Socket.IO, every component is designed to provide users with a seamless and enjoyable experience. The use of Express and MongoDB for backend and database management ensures scalability and performance, while the attractive UI enhances usability. Deploying the application on Render further simplifies the process, making it accessible to users at all times. Overall, this chat application is a comprehensive solution for real-time communication needs, offering security, functionality, and an exceptional user experience.
           
            
@@ -143,8 +143,8 @@ For users who have already signed up, the application offers a straightforward l
 
 
           <div className="text-wrap flex flex-col space-y-4">
-        <div className="text-3xl">What next ➡️</div>
-        <div className="text-xl">
+        <div className="sm:text-3xl text-2xl">What next ➡️</div>
+        <div className="sm:text-xl text-lg">
         Improvement in the chat application is continue........Task is to make it responsive 
         <br /> currently please open in the desktop mode. And upcoming changes consider to add media transfer and video call feature.
          </div>
