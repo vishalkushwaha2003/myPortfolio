5d96dfbfa06df46d3c03424b716b43b3620fa51a
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-14 01:05:45 +0530

    fix bugs

--- src/main/sidebar/Contact.jsx
+++ src/main/sidebar/Contact.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 // import Character3d from './Character3d'
 
 import ContactEmail from './ContactEmail'
-import ContactEmail1 from './ContactEmail1'
+
 
 
 function Contact() {
   return (
    
    <div className='w-full h-full  flex justify-center items-center'>
-     <ContactEmail1/>
+     <ContactEmail/>
    </div>
   )
 }
