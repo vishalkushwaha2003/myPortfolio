7fbd8c0019ac3b3f050873b7cb891056563777e7
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-14 00:31:34 +0530

    remove unnecessary code

--- src/main/sidebar/Character3d.jsx
+++ src/main/sidebar/Character3d.jsx
@@ -1,24 +1,24 @@
-// src/SplineViewer.js
-import React from 'react';
-import Loader from './Loader';
+// // src/SplineViewer.js
+// import React from 'react';
+// import Loader from './Loader';
 
-const Character3d = () => {
-  return (
-    <div className='sm:w-[60%] w-[90%] h-full m-auto overflow-hidden flex justify-center items-start'>
-      <spline-viewer 
-        hint 
-        url="https://prod.spline.design/Jp2fE2upCjWifr97/scene.splinecode"
-        logo={false}
-        height="100%"
-        width="100%"
+// const Character3d = () => {
+//   return (
+//     <div className='sm:w-[60%] w-[90%] h-full m-auto overflow-hidden flex justify-center items-start'>
+//       <spline-viewer 
+//         hint 
+//         url="https://prod.spline.design/Jp2fE2upCjWifr97/scene.splinecode"
+//         logo={false}
+//         height="100%"
+//         width="100%"
         
-      >
-        <div className=" w-full h-full flex justify-center items-center  ">
-          <Loader />
-        </div>
-      </spline-viewer>
-    </div>
-  );
-};
+//       >
+//         <div className=" w-full h-full flex justify-center items-center  ">
+//           <Loader />
+//         </div>
+//       </spline-viewer>
+//     </div>
+//   );
+// };
 
-export default Character3d;
+// export default Character3d;
--- src/main/sidebar/Contact.jsx
+++ src/main/sidebar/Contact.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import Character3d from './Character3d'
+// import Character3d from './Character3d'
 
-import ContactForm from './ContactForm'
 import ContactEmail from './ContactEmail'
 import ContactEmail1 from './ContactEmail1'
 
 
 function Contact() {
   return (
-    // <div className='w-full h-full grid grid-cols-12 grid-rows-2'>
-    //   <div className='col-span-12 row-span-1 overflow-hidden '><Character3d/></div>
-    //   <ContactForm/>
-      
-    // </div>
-    // <ContactEmail/>
+   
    <div className='w-full h-full  flex justify-center items-center'>
      <ContactEmail1/>
    </div>
